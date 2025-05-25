81fc7dcaf6116b025d8f8e795b648deac38bcd58
IvanMishi <i.a.m@inbox.ru>
2025-05-25 22:12:47 +0300

    origin

---
+++ test_modular_testing/users.py
@@ -0,0 +1,10 @@
+class Users:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+# Определение пользователей
+standard_user = User(username='standard_user', password='secret_sauce')
+locked_out_user = User(username='locked_out_user', password='secret_sauce')
+problem_user = User(username='problem_user', password='secret_sauce')
+performance_glitch_user = User(username='performance_glitch_user', password='secret_sauce')
