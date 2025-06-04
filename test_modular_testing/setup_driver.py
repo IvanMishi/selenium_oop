3cc32da40aacedaa2ec582bef567ece8774d237e
IvanMishi <i.a.m@inbox.ru>
2025-06-04 23:51:09 +0300

    upp

--- test_modular_testing/setup_driver.py
+++ test_modular_testing/setup_driver.py
@@ -5,15 +5,18 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class SetupDriver:
     def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.link = 'https://www.saucedemo.com/'
+        """Инициализация драйвера"""
+        self.driver = webdriver.Chrome() # Создает экземпляр ChromeDriver при инициализации объекта
+        self.link = 'https://www.saucedemo.com/' # URL тестируемого приложения
 
     def setup_driver(self):
-        self.driver.get(self.link) # Переходить по URL.
-        WebDriverWait(self.driver, 30).until(EC.url_to_be(self.link)) # Ожидает URl.
-        WebDriverWait(self.driver, 30).until(EC.title_contains("Swag Labs")) # Ожидает заголовок.
-        return self.driver # Возвращает драйвер для дальнейшего использования.
+        """Настройка драйвера"""
+        self.driver.get(self.link) # Переходит по ссылке
+        WebDriverWait(self.driver, 30).until(EC.url_to_be(self.link)) # Ожидает загрузку конкретного URL (до 30 секунд)
+        WebDriverWait(self.driver, 30).until(EC.title_contains("Swag Labs")) # Ожидае заголовок, с текстом "Swag Labs"
+        return self.driver # Возвращает настроенный драйвер для использования в других частях программы
 
     def close_driver(self):
+        """Безопасное завершение"""
         print('Браузер закрыт\n')
-        self.driver.quit() # Закрывает браузер.
+        self.driver.quit() # Закрывает браузер и освобождает ресурсы.
