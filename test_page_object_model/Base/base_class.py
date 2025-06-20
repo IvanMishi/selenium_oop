78cd3e2b632e73f2d1d5b7e42127332c05d2eeaa
IvanMishi <i.a.m@inbox.ru>
2025-06-20 23:28:03 +0300

    upp

--- test_page_object_model/Base/base_class.py
+++ test_page_object_model/Base/base_class.py
@@ -1,21 +1,25 @@
 
 
 
-
 class Base(): # Базовый класс в котором хранится драйвер он будет родительским, для всех наших последующих страниц Pages и будет содержать универсальные методы, которые мы будем в них использовать.
     """ Базовый класс, содержащий универсальные методы """
 
     def __init__(self, driver): # Передает драйвер который хранит экземпляр драйвера chrome
         self.driver = driver
+        self.url = 'https://www.saucedemo.com/'  # Обращение к URL из той страницы на которой она находится
 
-    """ Метод, возвращающий URL """
+    """ Метод, проверки текущего URL """
     def get_current_url(self): # Метод вызывается в классе LoginPage
         get_url = self.driver.current_url # Переменная которая получает url в текущей сессии
         print(f'Переходит по ссылке {get_url}') # Вывод значения переменной
+        print('Убеждается, что URL корректный.')
+        assert self.url == self.driver.current_url, f'Ожидаемый URL: {self.url}, Фактический URL: {self.driver.current_url}'
 
 
-        # print('Ожидает загрузку страницы.')
-        # WebDriverWait(self.driver, 60).until(EC.url_to_be(self.link))
-        # print('Убеждается, что URL корректный.')
-        # assert self.link == self.driver.current_url, f'Ожидаемый URL: {self.link}, Фактический URL: {self.driver.current_url}'
-        # fake = Faker("ru_Ru")  # Создаём экземпляр класса Faker, указывая, что хотим генерировать данные на (ru_Ru - русский язык)
+    """ Метод, проверка по ключевому слову """
+    def assert_word(self, word, result):  # Метод вызывается в классе LoginPage
+        value_word = word.text # Сохранение в переменную
+        assert value_word == result #Операция сравнения текста с результатом
+        print(f'Корректное значение {value_word}')
+
+
