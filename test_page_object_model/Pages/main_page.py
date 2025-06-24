45cdfb371c32d46fd5524d598bea7a482841bce9
IvanMishi <i.a.m@inbox.ru>
2025-06-24 22:49:44 +0300

    upp

--- test_page_object_model/Pages/main_page.py
+++ test_page_object_model/Pages/main_page.py
@@ -29,34 +29,52 @@ class MainPage(Base): # Наследование - класс потомок (в
 
 
 # ЛОКАТОРЫ. (Локаторы элементов, которые находятся на странице авторизации)
-    list_products = "inventory_item" # Локатор блока товаров на странице по CLASS_NAME .
-    button_shopping_cart  = "shopping_cart_badge"  # Локатор кнопки для перехода в козину на странице по CLASS_NAME .
+    list_products = "inventory_item" # Локатор блока товаров на странице по CLASS_NAME
+    button_shopping_cart  = "shopping_cart_badge"  # Локатор кнопки для перехода в козину на странице по CLASS_NAME
+    burger_menu = "react-burger-menu-btn" # Локатор ,бургер меню на странице по ID
+    link_about = '' # Локатор , ссылки 'about' на странице в бургер меню по ID
+
 
 
 # ГЕТТЕРЫ. (Методы, которые будут осуществлять поиск элементов, по ЛОКАТОРАМ, используя определенные условия поиска, и возвращающие результат данного поиска.)
     def get_list_products(self):
         return WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, self.list_products))) # Ищет карточки товаров на странице по указанному локатору вне метода через self и возвращает его значение далее.
-
     def get_button_shopping_cart(self):
         return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, self.button_shopping_cart))) # Ищет кнопку перехода в корзину на странице по указанному локатору вне метода через self и возвращает его значение далее.
-
+    def get_burger_menu(self):
+        return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, self.burger_menu))) # Ищет кнопку перехода в корзину на странице по указанному локатору вне метода через self и возвращает его значение далее.
+    def get_link_about(self):
+        return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, self.link_about)))  # Ищет кнопку перехода в корзину на странице по указанному локатору вне метода через self и возвращает его значение далее.
 
 
 # ДЕЙСТВИЯ. (Методы, которые будут принимать результат поиска от ГЕТТЕРОВ и производить требуемой действие, например кликать или вводить требуемую информациюв)
     def click_item_list_product(self):
         for item in self.get_list_products()[:2]: # Выбирает из списка товаров первые два
             item.find_element(By.CLASS_NAME, "btn").click()   # Вызывает метод на геттере через self и нажимает кнопку для добавления товара в корзину в корзину.
-
     def click_button_shopping_cart(self): #
         self.get_button_shopping_cart().click()  # Вызывает метод на геттере через self и нажимает кнопку для перехода в корзину.
         print('Нажимает кнопку авторизации на сайте')
-
+    def click_get_burger_menu(self): #
+        self.get_burger_menu().click()  # Вызывает метод на геттере через self и нажимает кнопку для перехода в корзину.
+        print('Нажимает кнопку бургер-меню на сайте')
+    def click_link_about(self): #
+        self.get_burger_menu().click()  # Вызывает метод на геттере через self и нажимает кнопку для перехода по ссылке.
+        print('Нажимает ссылку about в бургер-меню на сайте')
 
 
 # МЕТОДЫ. (Метод, содержащий список ДЕИИСТВИЙ, как шагов.)
+    # Метод для выбора продуктов
     def select_product(self):
         self.get_current_url()
         self.click_item_list_product() # Выбирает первые два товара
         self.click_button_shopping_cart() # Нажимает на кнопку перехода в корзину
-        self.assert_word(self.get_title_value(),'Your Cart')  # Вызов метода для подтверждения авторизации на сайте, resulе - значение с которым сравнивается.
+
+    # Метод для выбора в бургер-меню вкладки 'about'
+    def select_menu_about(self):
+        self.get_current_url()
+        self.click_get_burger_menu()
+        self.click_link_about()
+        self.assert_url('https://saucelabs.com/')
+
+
 
